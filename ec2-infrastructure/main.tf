ee12c1df40ad0390a444a90a1236b682e48dc085
danielkhan-v1 <grigorovich314@gmail.com>
2022-01-21 23:27:05 +0100

    add: EC2 instance terraform configuration

---
+++ ec2-infrastructure/main.tf
@@ -0,0 +1,112 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  token      = var.token
+  region     = "us-west-2"
+}
+
+resource "aws_vpc" "daniil-vpc" {
+  cidr_block           = "192.168.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "daniil-vpc"
+  }
+}
+
+resource "aws_subnet" "daniil-subnet" {
+  vpc_id                  = aws_vpc.daniil-vpc.id
+  cidr_block              = "192.168.0.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "daniil-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.daniil-vpc.id
+
+  tags = {
+    Name = "daniil-gateway"
+  }
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.daniil-vpc.id
+
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = true
+      to_port          = 0
+    },
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.daniil-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "daniil-rtb"
+  }
+}
+
+resource "aws_main_route_table_association" "main-rtb" {
+  vpc_id         = aws_vpc.daniil-vpc.id
+  route_table_id = aws_route_table.route-table.id
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-066333d9c572b0680"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.daniil-subnet.id
+
+  depends_on = [aws_internet_gateway.gateway]
+
+  tags = {
+    Name = "daniil-instance"
+  }
+}
