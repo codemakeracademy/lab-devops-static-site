00b9399a014d9951a07e64fb7f2f38f37afd8475
Mikhail <92841571+mivoronkov@users.noreply.github.com>
2022-01-25 19:17:41 +0300

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -24,40 +24,40 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = "mv-lab1234.s3.us-west-2.amazonaws.com"
-    origin_id   = "website"
-  }
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "website"
+#resource "aws_cloudfront_distribution" "s3_distribution" {
+ # origin {
+ #  domain_name = "mv-lab1234.s3.us-west-2.amazonaws.com"
+# origin_id   = "website"
+#  }
+#  viewer_certificate {
+#    cloudfront_default_certificate = true
+#  }
+#  default_cache_behavior {
+#    allowed_methods  = ["GET", "HEAD"]
+#    cached_methods   = ["GET", "HEAD"]
+#   target_origin_id = "website"
+#
+#   forwarded_values {
+#      query_string = false
 
-    forwarded_values {
-      query_string = false
+#      cookies {
+#        forward = "none"
+#      }
+#    }
 
-      cookies {
-        forward = "none"
-      }
-    }
+#    viewer_protocol_policy = "allow-all"
+#    min_ttl                = 0
+#   default_ttl            = 0
+#    max_ttl                = 0
+#    compress = true
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-    compress = true
-
-  }
-    enabled             = true
-    is_ipv6_enabled     = true
-    default_root_object = "index.html"
-    restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      }
-  }
-}
+#  }
+#    enabled             = true
+#    is_ipv6_enabled     = true
+#    default_root_object = "index.html"
+#    restrictions {
+#    geo_restriction {
+#      restriction_type = "none"
+#      }
+#  }
+#}
