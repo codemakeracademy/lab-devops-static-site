71eceecce7e02a545a30ec7cde20822ca8eceac6
danielkhan-v1 <grigorovich314@gmail.com>
2022-01-26 15:28:28 +0100

    fix: start_up script

--- dockerFiles/start_up.sh
+++ dockerFiles/start_up.sh
@@ -9,8 +9,6 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo shutdown -r now
-
 sudo docker-compose up -d
 
 docker update --restart unless-stopped jenkins-docker
