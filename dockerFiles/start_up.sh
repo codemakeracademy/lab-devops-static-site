e3bbc2699bf958ccf458264afb2aaed1536df09a
danielkhan-v1 <grigorovich314@gmail.com>
2022-01-26 15:30:41 +0100

    fix: start_up script

--- dockerFiles/start_up.sh
+++ dockerFiles/start_up.sh
@@ -9,7 +9,7 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo docker-compose up -d
+docker-compose up -d
 
 docker update --restart unless-stopped jenkins-docker
 docker update --restart unless-stopped sonarqube
