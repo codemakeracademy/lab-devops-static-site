73f444f01327365d91d8dab2044291fbd00e89f1
Maxim Trogaev <moriarty18.02@gmail.com>
2022-02-06 04:50:25 +0000

    feat: add EIP

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,49 +65,29 @@ resource "aws_security_group" "trogaev-sg" {
   }
 } 
 
+resource "aws_eip" "trogaev-eip" {
+  instance = aws_instance.trogaev-jenkins-lab.id
+  vpc      = true
+}
+
 resource "aws_key_pair" "trogaev_key" {
   key_name = "rp"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7E0QKlKmk97ZeNjl8pC0g898h7MqF8ci/YbamrYSkgAemVc5drIafHHvzi+njpjHUiOH7pPP/nS8l7bZSGPdQW+mYYcmMjm0AlqFdp84GTuSz15CQJmWyx03eLnWXfe4cKx6mdc3BqMQduF/E7/SeO3ZGOatzY766FOo97KSbPqc8mtb9HR2JhPwugCiEqYWB6o+sl3+9/1V7gc8PzHnJKJkd9nvvYSZ3bQuC2S715WQvRuxYs1SqBfBn5tknYPYcs8nvP+fUXj/5yiYsNNw1CrMlgRF9oD9EI/w9jgrxsxuleCEzcfmo8GX3iK+Bg4aqaIC2k+ImZ8gnROtiiEJL pi@raspberrypi"
 }
 
 resource "aws_instance" "trogaev-jenkins-lab" {
-  ami = "ami-066333d9c572b0680"
+  ami = "ami-0892d3c7ee96c0bf7"
   instance_type = "t3.large"
   subnet_id = aws_subnet.trogaev-subnet.id
   vpc_security_group_ids = [aws_security_group.trogaev-sg.id] 
   key_name = aws_key_pair.trogaev_key.key_name
-  iam_instance_profile = "jenkins"
+  #iam_instance_profile = "jenkins"
+  root_block_device {
+    volume_size = "20"
+  }
   tags = {
     Name = "trogaev-jenkins-lab"
   }
-}
-
-resource "aws_instance" "trogaev-jenkins" {
-  ami = "ami-066333d9c572b0680"
-  instance_type = "t3.medium"
-  subnet_id = aws_subnet.trogaev-subnet.id
-  vpc_security_group_ids = [aws_security_group.trogaev-sg.id] 
-  key_name = aws_key_pair.trogaev_key.key_name
-  iam_instance_profile = "jenkins"
-  tags = {
-    Name = "trogaev-jenkins"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "my_alarm" {
-    alarm_name          = "my_alarm"
-    comparison_operator = "LessThanOrEqualToThreshold"
-    evaluation_periods  = 12
-    metric_name         = "CPUUtilization"
-    namespace           = "AWS/EC2"
-    period              = 300
-    statistic           = "Average"
-    threshold           = 10
-    alarm_description = "Stop the EC2 instance when CPU utilization stays below 10% on average for 12 periods of 5 minutes, i.e. 1 hour"
-    alarm_actions     = ["arn:aws:automate:us-west-2:ec2:stop"]
-    dimensions = {
-        InstanceId = "i-0fbd165043e36ff4c"
-    }
 }
 
 resource "aws_cloudwatch_metric_alarm" "trogaev_alarm" {
